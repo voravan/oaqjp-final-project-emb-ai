aabef39b4c27014a98955fda138fda58cd0d2e3c
voravan <voravan@gmail.com>
2026-06-18 06:31:42 +0700

    Create 7a_error_handling_function.py

---
+++ 7a_error_handling_function.py
@@ -0,0 +1,30 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+    URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_json = { "raw_document": { "text": text_to_analyze } }
+    
+    response = requests.post(URL, json=input_json, headers=header)
+    
+    if response.status_code == 200:
+        formatted_response = json.loads(response.text)
+        emotions = formatted_response['emotionPredictions'][0]['emotion']
+        
+        anger = emotions['anger']
+        disgust = emotions['disgust']
+        fear = emotions['fear']
+        joy = emotions['joy']
+        sadness = emotions['sadness']
+        max_emotion = max(emotions, key=emotions.get)
+        
+        return {
+            'anger': anger, 'disgust': disgust, 'fear': fear,
+            'joy': joy, 'sadness': sadness, 'dominant_emotion': max_emotion
+        }
+    elif response.status_code == 400:
+        return {
+            'anger': None, 'disgust': None, 'fear': None,
+            'joy': None, 'sadness': None, 'dominant_emotion': None
+        }
