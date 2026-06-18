a3206687fb20770719d8c3583c2cc66e387a671a
voravan <voravan@gmail.com>
2026-06-18 10:05:52 +0700

    Update 6a_server.py

--- 6a_server.py
+++ 6a_server.py
@@ -1,13 +1,24 @@
+"""
+Flask server interface running the Emotion Detection Application.
+"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
-app = Flask("Emotion Detector Server")
+app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def emot_detector():
+    """
+    Receives text queries via HTTP requests, submits them for evaluation,
+    and structures string rendering responses back to the viewport.
+    """
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
     
+    # Simple check for valid response structure
+    if response['dominant_emotion'] is None:
+        return "Invalid text! Please try again!"
+
     return (
         f"For the given statement, the system response is "
         f"'anger': {response['anger']}, 'disgust': {response['disgust']}, "
@@ -18,6 +29,9 @@ def emot_detector():
 
 @app.route("/")
 def render_index_page():
+    """
+    Renders the default landing home route template page interface.
+    """
     return render_template('index.html')
 
 if __name__ == "__main__":
